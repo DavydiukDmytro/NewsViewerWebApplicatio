cb7fdb7f1687e82ba1c86f5dfdc8d6961d5d4567
davudyuck <69296072+davudyuck@users.noreply.github.com>
2023-03-23 06:21:26 +0000

    Deploying to gh-pages from @ davudyuck/NewsViewerWebApplicatio@11f17b52f050932f64bc9723ee363e5cb2e09455 🚀

---
+++ favorite.17af98d9.js
@@ -0,0 +1,2 @@
+console.log({section:"admin",display_name:"Admin"});
+//# sourceMappingURL=favorite.17af98d9.js.map
---
+++ favorite.3c427c6a.js
@@ -0,0 +1,2 @@
+console.log({section:"admin",display_name:"Admin"});
+//# sourceMappingURL=favorite.3c427c6a.js.map
