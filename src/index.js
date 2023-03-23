25eb1170a502ca8813cc0596e5be343eace0ccfe
kirhal <kirillgaleckiy@gmail.com>
2023-03-23 17:49:37 +0200

    et

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { category } from './js/category';
 import { Requests } from './js/requests';
+import { markup, newObj } from './js/markup';
 
 const API_URL_NEWS = 'https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json';
 const KEY_NEWS = '1XlCr4gRqRG4oQXZ0w6Bhmx7Lrq32aXd';
@@ -9,9 +10,14 @@ const requestsNews = new Requests(API_URL_NEWS, KEY_NEWS);
 
 //робить запит за адресою та виводить в консоль
 try {
-    const newsPopular = requestsNews.getRequests(requestsNews.createTrendingNewsQueryUrl());
-    newsPopular.then(value => console.log(value.results));
+  const newsPopular = requestsNews.getRequests(
+    requestsNews.createTrendingNewsQueryUrl()
+  );
+  newsPopular.then(value => {
+    console.log(value.results);
+    const qqq = markup(value);
+    newObj(value);
+  });
 } catch (error) {
-    console.log(error.message);
+  console.log(error.message);
 }
-
---
+++ src/js/markup.js
@@ -0,0 +1,49 @@
+let news = [];
+let conct = [];
+const book = {
+  title: 'The Last Kingdom',
+  author: 'Bernard Cornwell',
+  genres: ['historical prose', 'adventure'],
+  isPublic: true,
+  rating: 8.38,
+};
+
+export function newObj(value) {
+  const xxx = value.results;
+  xxx.forEach(function ({
+    title,
+    asset_id,
+    url,
+    published_date,
+    abstract,
+    media,
+    flag = 'news',
+    favorite = false,
+    read = false,
+  }) {
+    news.push({
+      asset_id,
+      published_date,
+      title,
+      abstract,
+      url,
+      media,
+      flag,
+      favorite,
+      read,
+    });
+  });
+  console.log('News Arr : ', news);
+}
+
+export function markup(value) {
+  console.log(value);
+  const xxx = value.results;
+  return xxx
+    .map(
+      obj =>
+        `<img src="https://static01.nyt.com/images/2023/03/22/multimedia/22trump-grandjury-cvkg/22trump-grandjury-cvkg-mediumThreeByTwo440.jpg"/>
+    <h1>${obj.title}</h1><p>${obj.abstract}</p><div><p></p><a href=${obj.url}>Read more</a></div>`
+    )
+    .join('');
+}
