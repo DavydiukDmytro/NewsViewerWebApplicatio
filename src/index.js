356e207d8b89fbe6839d1560f51adb13dc8884a8
kirhal <kirillgaleckiy@gmail.com>
2023-03-25 22:13:17 +0200

    doen

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { category } from './js/category';
 import { Requests } from './js/requests';
 import { requestsWeatherPosition, fetchWeather } from './js/weather';
-import { concatNewsAndWeather, newsMarkup } from './js/markup';
+import { concatNewsAndWeather, createMarkUp } from './js/markup';
 
 const API_URL_NEWS = 'https://api.nytimes.com/svc';
 const KEY_NEWS = '1XlCr4gRqRG4oQXZ0w6Bhmx7Lrq32aXd';
@@ -66,8 +66,8 @@ async function searchPopular() {
     );
     console.log('Concated arr popular:', arrayCardNews);
     // ===Розмітка новин і погоди============================
-    // const markup = newsMarkup(arrayCardNews);
-    // console.log(markup);
+    const markup = createMarkUp(arrayCardNews);
+    console.log(markup);
     //тимчасово видалить потом
     // console.log(arrayPopuralNews);
   } catch (error) {
@@ -92,6 +92,8 @@ async function searchArticle(searchValue) {
       weather
     );
     console.log('Concated arr search:', arrayCardNews);
+    const markup = createMarkUp(arrayCardNews);
+    console.log(markup);
   } catch (error) {
     console.error(error);
   }
--- src/js/markup.js
+++ src/js/markup.js
@@ -11,6 +11,18 @@ export function concatNewsAndWeather(incomeArr, favoriteArr, readedArr, obj) {
   return concatArray;
 }
 
+export function createMarkUp(array) {
+  return array
+    .map(function (elem) {
+      if (elem.flag === 'news') {
+        return newsMarkUp(elem);
+      } else {
+        return weatherMarkUp(elem);
+      }
+    })
+    .join('');
+}
+
 function checkArrays(incomeArr, favoriteArr, readedArr) {
   let checkedArray = [];
   for (let i = 0; i < incomeArr.length; i++) {
@@ -29,7 +41,6 @@ function checkArrays(incomeArr, favoriteArr, readedArr) {
   }
   return checkedArray;
 }
-
 function dataDestructuring(elem) {
   if (elem.asset_id) {
     const {
@@ -79,119 +90,61 @@ function dataDestructuring(elem) {
     };
   }
 }
-
-export function newsMarkup(array) {
-  return array
-    .map(function (elem) {
-      if (elem.flag === 'news') {
-        return `
-        <li>
-          <img src="https://static01.nyt.com/images/2023/03/22/multimedia/22trump-grandjury-cvkg/22trump-grandjury-cvkg-mediumThreeByTwo440.jpg"/>
-            <h1>${elem.title}</h1>
-            <p>${elem.abstract}</p>
-              <div>
-                <p></p>
-                <a href=${elem.url}>Read more</a>
-              </div>
-        </li>`;
-      } else {
-        return `<li class="weather__card">
-              <div class="weather__wrapper">
-                <p class="weather__temperature">${elem.temp}</p>
-                <div class="weather__box">
-                  <p class="weather__description">${elem.descriptrion}</p>
-                  <button class="weather__button">
-                    <svg
-                      width="18px"
-                      height="18px"
-                      class="weather__location-icon"
-                    >
-                    <use href="#"></use> ===ПРОПИСАТИ===
-                  </svg>
-                  ${elem.city}
-                </button>
-              </div>
-            </div>
-          <img class="weather-picture" src="https://openweathermap.org/img/wn/${elem.icon}@2x.png"></img>
-            <p class="weather__day">${elem.dayWeek}</p>
-            <p class="weather__date">${elem.date}</p>
-        </li>`;
-      }
-    })
-    .join('');
+function newsMarkUp({
+  abstract,
+  id,
+  published_date,
+  url,
+  title,
+  media,
+  flag,
+  favorite,
+  read,
+}) {
+  return `<li class="news-card">
+    <div class="news-card__image">
+      <img src="#" alt="News" />
+      <span class="news-card__category">Job searching</span>
+      <span class="news-card__status">Have read</span>
+      <button class="news-card__favorite">
+        Add to favorite
+        <svg class="news-card__icon" width="16px" height="16px">
+          <use href="./img/icons.svg#icon-favorite"></use>
+        </svg>
+      </button>
+    </div>
+    <h2 class="news-card__title">
+      ${title}
+    </h2>
+    <p class="news-card__text">
+      ${abstract}
+    </p>
+    <div class="news-card__box">
+      <span class="news-card__date">${published_date}</span>
+      <a class="news-card__read">Read more</a>
+    </div>
+  </li>;`;
 }
-
-// export function TEST(incomeArr, favoriteArr, readedArr, obj) {
-//   const concatArray = [];
-//   const checkedArray = checkArrays(incomeArr, favoriteArr, readedArr);
-//   console.log('Cheked arr:', checkedArray);
-//   checkedArray.forEach(function (elem, index) {
-//     concatArray.push(dataDestructuring(elem));
-//     if (index === 1 && obj.flag === 'weather') {
-//       concatArray.push(obj);
-//     }
-//   });
-//   return concatArray;
-// }
-
-// export function concatNewsAndWeather(incomeArr, favoriteArr, readedArr, obj) {
-//   const concatArray = [];
-//   const checkedArray = checkArrays(incomeArr, favoriteArr, readedArr);
-//   console.log('Cheked arr:', checkedArray);
-//   checkedArray.forEach(function (
-//     {
-//       abstract,
-//       title,
-//       asset_id: id,
-//       url,
-//       published_date,
-//       media,
-//       flag = 'news',
-//       favorite = false,
-//       read = false,
-//     },
-//     index
-//   ) {
-//     concatArray.push({
-//       abstract,
-//       id,
-//       published_date,
-//       title,
-//       url,
-//       media,
-//       flag,
-//       favorite,
-//       read,
-//       _id,
-//     });
-//     if (index === 1 && obj.flag === 'weather') {
-//       concatArray.push(obj);
-//     }
-//   });
-//   return concatArray;
-// }
-
-/* <li class="news-card">
-  <div class="news-card__image">
-    <img src="#" alt="News" />
-    <span class="news-card__category">Job searching</span>
-    <span class="news-card__status">Have read</span>
-    <button class="news-card__favorite">
-      Add to favorite
-      <svg class="news-card__icon" width="16px" height="16px">
-        <use href="./img/icons.svg#icon-favorite"></use>
-      </svg>
-    </button>
+function weatherMarkUp({ temp, descriptrion, city, icon, dayWeek, date }) {
+  return `<li class="weather__card">
+    <div class="weather__wrapper">
+      <p class="weather__temperature">${temp}</p>
+      <div class="weather__box">
+        <p class="weather__description">${descriptrion}</p>
+        <button class="weather__button">
+          <svg
+            width="18px"
+            height="18px"
+            class="weather__location-icon"
+          >
+          <use href="#"></use> ===ПРОПИСАТИ===
+        </svg>
+        ${city}
+      </button>
+    </div>
   </div>
-  <h2 class="news-card__title">
-    8 tips for passing an online interview that will help you get a job
-  </h2>
-  <p class="news-card__text">
-    Before you start looking for a job, it is useful to familiarize yourself
-    with the job prospects offered by these...
-  </p>
-  <div class="news-card__box">
-    <span class="news-card__date">20/02/2021</span>
-    <a class="news-card__read">Read more</a>
-  </div>
-</li>; */
+<img class="weather-picture" src="https://openweathermap.org/img/wn/${icon}@2x.png"></img>
+  <p class="weather__day">${dayWeek}</p>
+  <p class="weather__date">${date}</p>
+</li>`;
+}
