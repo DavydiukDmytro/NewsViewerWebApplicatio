ccd9c66f9a68c77067c3023b908a8570b99f2b9d
Roman-Shvets <rshvets72@gmail.com>
2023-03-26 15:10:28 +0300

    Перемикач теми: світла/темна

--- src/index.js
+++ src/index.js
@@ -67,4 +67,25 @@ function onClickSearchBtn(e) {
   searchArticle(encodeURIComponent('The New York Times'));
 }
 
+//Перемикач теми - темна/світла
+const LOCALSTORAGE_KEY = 'theme';
+let themeLight = true;
+const selectTheme = document.querySelector('#theme-clicker');
+const element = document.querySelector('body');
+selectTheme.addEventListener("change", setTheme);
 
+themeLight = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+if (!themeLight) {
+  element.classList.add("dark");
+  selectTheme.checked = true;
+}
+else {
+  selectTheme.checked = false;
+}
+
+function setTheme () {
+  element.classList.toggle("dark");
+  themeLight = !themeLight;
+  localStorage.setItem(LOCALSTORAGE_KEY, themeLight);
+}
