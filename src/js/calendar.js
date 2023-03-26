95634022019199e4cccf6a4e340d1271a822e244
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-03-26 21:23:30 +0300

    calendar.js

---
+++ src/js/calendar.js
@@ -0,0 +1,11 @@
+import flatpickr from 'flatpickr';
+const flatpickr = require('flatpickr');
+
+flatpickr('#calendar', {
+  altFormat: 'F j, Y',
+  dateFormat: 'd/m/Y',
+});
+
+// const calendarHidden = document.querySelectorAll('flatpickr-calendar');
+// calendarHidden.classList.add('visually-hidden');
+// return calendarHidden;
