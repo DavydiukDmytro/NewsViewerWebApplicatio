b30a7752cb1f02b909e46a4b2aee142df0319290
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-03-26 22:18:48 +0300

    add corect calendar

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -1,11 +1,26 @@
 import flatpickr from 'flatpickr';
 const flatpickr = require('flatpickr');
 
-flatpickr('#calendar', {
-  altFormat: 'F j, Y',
-  dateFormat: 'd/m/Y',
+// flatpickr('#calendar', {
+//   altFormat: 'F j, Y',
+//   dateFormat: 'd/m/Y',
+// });
+
+const dateInput = document.getElementById('date-input');
+
+flatpickr(dateInput, {
+  onChange: function (selectedDates, dateStr, instance) {
+    const selectedDate = selectedDates[0];
+    console.log(selectedDate);
+  },
 });
 
-// const calendarHidden = document.querySelectorAll('flatpickr-calendar');
-// calendarHidden.classList.add('visually-hidden');
-// return calendarHidden;
+// const myInput = document.querySelector('.myInput');
+// const fp = flatpickr(myInput, {});
+
+// const calendarWrapper = document.querySelectorAll('calendar__wrapper');
+// const calendar = document.querySelectorAll('flatpickr-calendar');
+
+// calendarWrapper.addEventListener('click', function () {
+//   calendar.classList.toggle('visible');
+// });
