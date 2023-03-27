9ffe810b5a6db5572ce915ae9af9453a64824670
davudyuck <69296072+davudyuck@users.noreply.github.com>
2023-03-28 00:17:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import { clearNewsSection } from './js/clear-news-section';
 //import функції відображання помилки та її зникнення
 import { showPageNotFound, hidePageNotFound } from './js/not-found';
 //import функції яка повертає значення вибраної категорії
-import { selectedCategory } from './js/selected-category';
+// import { selectedCategory } from './js/selected-category';
 //Лодаш троттле
 import throttle from 'lodash.throttle';
 // Додав функцію яка записую і повертає данні з localStorage
