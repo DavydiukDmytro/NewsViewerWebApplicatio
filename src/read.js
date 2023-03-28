26a9ec9c7407c3c766ec25ce9fdb703a13bb89e4
davudyuck <davudyuck0501@gmail.com>
2023-03-29 02:03:20 +0300

    fix text favoriet

--- src/read.js
+++ src/read.js
@@ -17,6 +17,8 @@ let arrayCardNewsReadStorage = arrayNews.map(element => {
   element.read = 'read';
   return element
 });
+let arrayCardNewsFavorite = [];
+arrayCardNewsFavorite = load('favorite');
 
 accordionEl.innerHTML = createAccordionHeadlines(arrayCardNewsReadStorage); // <=Change argument
 
@@ -30,6 +32,73 @@ const accordion = new Accordion(accordionsArr, {
     
 if (arrayCardNewsReadStorage.length < 1) {
   console.log('erorr');
+}
+const sectionReadRefs = document.querySelector('.section-news');
+sectionReadRefs.addEventListener('click', onClickInSectionNews);
+function onClickInSectionNews(e) {
+  const button = e.target.closest('button');
+  const li = e.target.closest('li');
+  if (button) {
+    const buttonId = button.dataset.id;
+    const buttonDataAttribute = button.dataset.favorite;
+    if (buttonDataAttribute === 'true') {
+      const indexCard = arrayCardNewsFavorite.findIndex(card => String(card.id) === buttonId);
+      arrayCardNewsFavorite.splice(indexCard, 1);
+      button.children[1].dataset.favorite = 'false';
+      button.children[2].dataset.favorite = 'false';
+      button.dataset.favorite = 'false';
+      button.children[0].textContent = 'Add to favorite';
+      save('favorite', arrayCardNewsFavorite);
+      let array1 = arrayCardNewsReadStorage;
+      arrayCardNewsReadStorage = array1.map(element => {
+        element.read = "true";
+        if (String(element.id) === buttonId) {
+          element.favorite = "false";
+        }
+        return element
+      });
+      save('read', arrayCardNewsReadStorage);
+    } else {
+      const cardNewFavorite = arrayCardNewsReadStorage.find(card => String(card.id) === buttonId);
+      cardNewFavorite.favorite = true;
+      cardNewFavorite.read = "true";
+      arrayCardNewsFavorite.push(cardNewFavorite);
+      button.children[1].dataset.favorite = 'true';
+      button.children[2].dataset.favorite = 'true';
+      button.dataset.favorite = 'true';
+      button.children[0].textContent = 'Remove from favoriet';
+      save('favorite', arrayCardNewsFavorite);
+      let array1 = arrayCardNewsReadStorage;
+      arrayCardNewsReadStorage = array1.map(element => {
+        element.read = "true";
+        if (String(element.id) === buttonId) {
+          element.favorite = "true";
+        }
+        return element
+      });
+      save('read', arrayCardNewsReadStorage);
+    }
+  }
+  if (e.target.nodeName === 'A') {
+    const linkId = e.target.dataset.ida;
+    const linkDataAttribute = e.target.dataset.read;
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+
+    const formattedDate = `${year}-${month}-${day}`;
+    if (linkDataAttribute === 'true') {
+      const indexCard = arrayCardNewsReadStorage.findIndex(card => String(card.id) === linkId);
+      arrayCardNewsReadStorage[indexCard].readed_date = formattedDate;
+      let array1 = arrayCardNewsReadStorage;
+      arrayCardNewsFavorite = array1.map(element => {
+        element.read = "true";
+        return element;
+      })
+      save('read', arrayCardNewsReadStorage);
+    }
+  }
 }
 
 
